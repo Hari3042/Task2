67f318ccbf8d01a4d555240c1914daf962fe6c76
Hari3042 <131939194+Hari3042@users.noreply.github.com>
2023-05-02 17:23:14 +0530

    Update Locations.sh

--- Locations.sh
+++ Locations.sh
@@ -3,7 +3,7 @@
 sudo mkdir /opt/Applications
 sudo mv ~/locations-postgres.jar /opt/Applications/locations-postgres.jar
 
-# set environment   
+# set environment       
 echo 'PG_DB=postgresdb
 PG_USER=postgresuser
 PG_PASSWORD=password
